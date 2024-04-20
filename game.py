b160c7c3ee4b5d077f19560b01facc6ce13ba0ad
Ying Zhang <zhangjying09@gmail.com>
2024-04-20 17:01:09 -0400

    play button

---
+++ game.py
@@ -0,0 +1,73 @@
+import pygame
+from models import *
+from engine import *
+
+screen_height = 768
+screen_width = 1024
+bounds = (screen_width, screen_height)
+window = pygame.display.set_mode(bounds)
+pygame.display.set_caption("Rat Slapper")
+
+def game_screen():
+    gameEngine = RatEngine()
+
+    # Load back of card image
+    cardBack = pygame.image.load('images/BACK.png')
+    cardBack = pygame.transform.scale(cardBack, (int(238*0.8), int(332*0.8)))
+
+    # Draws appropriate UI to window based on state and result of gameEngine
+    def renderGame(window):
+        # Draw general UI
+        window.fill((15,0,169))
+        font = pygame.font.SysFont('comicsans',60, True)
+
+        window.blit(cardBack, (100, 200))
+        window.blit(cardBack, (700, 200))
+
+        text = font.render(str(len(gameEngine.player1.hand)) + " cards", True, (255,255,255))
+        window.blit(text, (100, 500))
+
+        text = font.render(str(len(gameEngine.player2.hand)) + " cards", True, (255,255,255))
+        window.blit(text, (700, 500))
+
+        topCard = gameEngine.pile.peek()
+        if (topCard != None):
+            window.blit(topCard.image, (400, 200))
+
+        # Indicate current state of game and whose turn it is
+        if gameEngine.state == GameState.PLAYING:
+            text = font.render(gameEngine.currentPlayer.name + " to flip", True, (255,255,255))
+            window.blit(text, (20,50))
+
+        if gameEngine.state == GameState.SLAPPING:
+            result = gameEngine.result
+            if result["isSlap"] == True:
+                message = "Winning Slap! by " + result["winner"].name
+            else:
+                message = "False Slap! by " + result["slapCaller"].name + ". " + result["winner"].name + " wins!"
+            text = font.render(message, True, (255,255,255))
+            window.blit(text, (20,50))
+
+        if gameEngine.state == GameState.ENDED:
+            result = gameEngine.result
+            message = "Game Over! " + result["winner"].name + " wins!"
+            text = font.render(message, True, (255,255,255))
+            window.blit(text, (20,50))
+
+    # Run Game Loop (Listen for User Input, Process Input w/ Engine Play Method, Update UI w/ Result)
+    run = True
+    while run:
+        key = None;
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                key = event.key
+
+        gameEngine.play(key)
+        renderGame(window)
+        pygame.display.update()
+
+        if gameEngine.state == GameState.SLAPPING:
+            pygame.time.delay(3000)
+            gameEngine.state = GameState.PLAYING
---
+++ home.py
@@ -0,0 +1,60 @@
+import pygame
+import button
+from game import * 
+
+pygame.init()
+
+screen_height = 768
+screen_width = 1024
+bounds = (screen_width, screen_height)
+window = pygame.display.set_mode(bounds)
+pygame.display.set_caption("Rat Slapper")
+
+
+def main_menu():
+	# logo image
+	logo_img = pygame.image.load('images/logo.png')
+	logo_img = pygame.transform.scale(logo_img, (850, 300))
+
+	#load button images
+	play_img = pygame.image.load('images/play.png').convert_alpha()
+	rules_img = pygame.image.load('images/rules.png').convert_alpha()
+
+	#create button instances
+	play_button = button.Button(100, 400, play_img, 0.5)
+	rules_button = button.Button(600, 400, rules_img, 0.5)
+	
+	window.fill((202, 228, 241))
+	window.blit(logo_img, (100,100))
+
+	# game loop 
+	run = True 
+	while run:
+
+		MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+		if play_button.draw(window):
+			print('PLAY')
+		if rules_button.draw(window):
+			print('RULES')
+
+		#event handler
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if play_button.checkForInput(MENU_MOUSE_POS):
+					play_game()
+				# if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+				# 	options()
+				# if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+				# 	pygame.quit()
+				# 	sys.exit()
+
+		pygame.display.update()
+
+def play_game():
+	game_screen()
+
+main_menu()
+pygame.quit()
