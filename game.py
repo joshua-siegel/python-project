d7acb0aabe9abc0ddee5ac80f09140f2c0bd7d0c
Ying Zhang <zhangjying09@gmail.com>
2024-04-28 15:18:53 -0400

    Player key labels

--- game.py
+++ game.py
@@ -23,7 +23,8 @@ def game_screen():
     def renderGame(window):
         # Draw general UI
         window.fill((202, 228, 241))
-        font = pygame.font.SysFont('comicsans',60, True)
+        font = pygame.font.SysFont('comicsans',50, True)
+        font2 = pygame.font.SysFont('comicsans',25, True)
 
         window.blit(cardBack, (100, 200))
         window.blit(cardBack, (700, 200))
@@ -34,6 +35,16 @@ def game_screen():
         text = font.render(str(len(gameEngine.player2.hand)) + " cards", True, (255,255,255))
         window.blit(text, (700, 500))
 
+        # Player Key Directions
+        Player1_label = font2.render("Player 1", True, (255,255,255))
+        Player1_keys = font2.render("Q = Flip, W = Slap", True, (255,255,255))
+        Player2_label = font2.render("Player 2", True, (255,255,255))
+        Player2_keys = font2.render("O = Flip, P = Slap", True, (255,255,255))
+        window.blit(Player1_keys, (100, 700))
+        window.blit(Player2_keys, (700, 700))
+        window.blit(Player1_label, (100, 650))
+        window.blit(Player2_label, (700, 650))
+
         topCard = gameEngine.pile.peek()
         if (topCard != None):
             window.blit(topCard.image, (400, 200))
@@ -41,7 +52,7 @@ def game_screen():
         # Indicate current state of game and whose turn it is
         if gameEngine.state == GameState.PLAYING:
             text = font.render(gameEngine.currentPlayer.name + " to flip", True, (255,255,255))
-            window.blit(text, (20,50))
+            window.blit(text, (100,50))
 
         if gameEngine.state == GameState.SLAPPING:
             result = gameEngine.result
@@ -50,19 +61,19 @@ def game_screen():
             else:
                 message = "False Slap! by " + result["slapCaller"].name + ". " + result["winner"].name + " wins!"
             text = font.render(message, True, (255,255,255))
-            window.blit(text, (20,50))
+            window.blit(text, (100,50))
 
         if gameEngine.state == GameState.FACE:
             result = gameEngine.result
             message = result["winner"].name + " wins by face card played!"
             text = font.render(message, True, (255,255,255))
-            window.blit(text, (20,50))
+            window.blit(text, (100,50))
 
         if gameEngine.state == GameState.ENDED:
             result = gameEngine.result
             message = "Game Over! " + result["winner"].name + " wins!"
             text = font.render(message, True, (255,255,255))
-            window.blit(text, (20,50))
+            window.blit(text, (100,50))
 
     # Run Game Loop (Listen for User Input, Process Input w/ Engine Play Method, Update UI w/ Result)
     run = True
